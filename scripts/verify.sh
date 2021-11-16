656bf192abec8683955c79df4bfd54337aa581ff
Kirill <g4s8.public@gmail.com>
2021-11-16 10:12:23 +0300

    ci: verify adapters on change

---
+++ scripts/verify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+mvn install
+
+target=$1
+testdir=$(mktemp --directory)
+
+git clone --depth=1 https://github.com/${target}.git $testdir
+cd $testdir
+
+sed  '0,/<version>/s/<version>.*<\/version>/<version>1.0-SNAPSHOT<\/version>/' -i pom.xml
+mvn verify -Pqulice
