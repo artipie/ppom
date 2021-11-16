be02b72cc30cf3a6ab01a30042a1ffb141330d26
Kirill <g4s8.public@gmail.com>
2021-11-16 11:33:32 +0300

    ci: build src and test for verify

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -7,8 +7,15 @@ mvn install -B -q
 target=$1
 testdir=$(mktemp --directory)
 
+function cleanup {
+  rm -fr $testdir
+}
+
+trap cleanup EXIT
+
 git clone --depth=1 https://github.com/${target}.git $testdir
 cd $testdir
 
 sed  '0,/<version>/s/<version>.*<\/version>/<version>1.0-SNAPSHOT<\/version>/' -i pom.xml
-mvn verify -B -q -Pqulice
+mvn compile -B
+mvn test-compile -B
